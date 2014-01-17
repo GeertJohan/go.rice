a98da77de12095e9a560cb9513a4b47ce4ce7164
GeertJohan <gjr19912@gmail.com>
2014-01-17 20:30:12 +0100

    Add verbose logging for clean operation

--- rice/clean.go
+++ rice/clean.go
@@ -9,12 +9,14 @@ import (
 
 func operationClean(pkg *build.Package) {
 	for _, filename := range pkg.GoFiles {
+		verbosef("checking file '%s'\n", filename)
 		if strings.HasSuffix(filename, ".rice-box.go") || strings.HasSuffix(filename, ".rice-single.go") {
 			err := os.Remove(filename)
 			if err != nil {
 				fmt.Printf("error removing file (%s): %s\n", filename, err)
 				os.Exit(-1)
 			}
+			verbosef("removed file '%s'\n", filename)
 		}
 	}
 }
